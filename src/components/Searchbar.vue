e9763a21ad48853fe4acb7b6b3be9ef35890490a
KyleOkwach <bkyle9023@gmail.com>
2022-06-15 22:51:58 +0300

    Ui changes

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -15,9 +15,9 @@ export default{
             class="shadow-xl"
         >
             <input type="text" v-model="inp.value" 
-                class="rounded-l-xl">
+                class="rounded-l-2xl w-[30vw] h-7">
             <button class="bg-gray-600 hover:bg-gray-500 
-                rounded-r-xl px-1 transition-all 
+                rounded-r-2xl px-2 p-[3px] transition-all 
                 duration-300 text-white
             "
             >
